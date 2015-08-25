3d78b36ec99d251d27832013e4e578c10cdd8194
Pantamis <theo.saillant@free.fr>
2015-08-25 20:19:46 +0200

    Added warning GPL

--- launchkey 61.control.js
+++ launchkey 61.control.js
@@ -1,4 +1,19 @@
-
+//    Controller Script for Launchkey series in Biwig Studio
+//    Copyright (C) 2015  SAILLANT
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 loadAPI(1);
 
 host.defineController("Novation", "Launchkey 61", "1.0", "45108550-9E02-11E2-9E96-D0AA527BA73B");
